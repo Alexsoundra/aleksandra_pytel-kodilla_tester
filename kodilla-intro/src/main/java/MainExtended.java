de1a1c0da067c911d5c19fa9a8e79918decb52d7
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-05-03 13:01:54 +0200

    zadanie 2

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String []args) {
+        String myName = "Aleksandra";
+        System.out.println(myName);
+        int myAge = 30;
+        System.out.println(myAge);
+        boolean greatTester = true;
+        System.out.println(greatTester);
+        char firstLetter = 'A';
+        System.out.println(firstLetter);
+        double myHeight = 1.70;
+        System.out.println(myHeight);
+        String theEnd = "Have a great day!";
+        System.out.println(theEnd);
+    }
+}
