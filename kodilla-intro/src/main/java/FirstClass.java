fabdd3d4029e44bb8effdfc4241e05f110e2037c
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-05-03 12:59:24 +0200

    zadanie 1

---
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -0,0 +1,12 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello from FirstClass!");
+        String example = ("Hello from FirstClass!");
+        System.out.println(example);
+        if (1 > 2){
+            System.out.println("Niesamowite! Jeden jest większe niż dwa!!!");
+        } else {
+            System.out.println("Wszystko po staremu, jeden jest mniejsze niż dwa.");
+        }
+    }
+}
---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("myName = Maciej");
+        int myAge = (int) 22.1;
+        int height = (int) 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+    }
+}
+
