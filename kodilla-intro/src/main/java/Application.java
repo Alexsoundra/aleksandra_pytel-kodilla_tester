52fe88d4c5332521024d72583dec5c4d359c55c1
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-05-18 16:22:49 +0200

    zadanie Application

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,28 +1,24 @@
 public class Application {
-        String name;
-        double age;
-        double height;
-
-        public Application(String name, double age, double height){
-            this.name = name;
-            this.age = age;
-            this.height = height;
+    public static String check(Person person) {
+        String userInformation = null;
+        //wg mnie if jest zbyt ubogo opisany, mógłby zawierać 2xelse if
+        if (person.name != null) {
+            if (person.age > 30 && person.height > 160) {
+                userInformation = "User is older than 30 and higher then 160cm";
+            }
+            else {
+                userInformation = "User is younger than 30 or lower than 160cm";
+            }
         }
-
-         public void calculateUserAgeAndHeight() {
-             if (age > 30 && height > 160) {
-                 System.out.println("User is older than 30 and higher then 160cm");
-             } else {
-                 System.out.println("User is younger than 30 or lower than 160cm");
-             }
-         }
-    public static void main(String[] args){
-        Application adam = new Application("Adam", 40.5, 178);
-        adam.calculateUserAgeAndHeight();
-        Application kora = new Application("Kora", 15, 166);
-        kora.calculateUserAgeAndHeight();
-        Application daniel = new Application("Daniel", 31, 180);
-        daniel.calculateUserAgeAndHeight();
+        return userInformation;
+    }
+    public static void main(String[] args) {
+        Person adam = new Person("Adam", 40.5, 178);
+        Person kora = new Person("Kora", 15, 166);
+        Person daniel = new Person("Daniel", 31, 180);
+       System.out.println(Application.check(adam));
+       System.out.println(Application.check(kora));
+       System.out.println(Application.check(daniel));
     }
 }
 
---
+++ kodilla-intro/src/main/java/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String name;
+    double age;
+    double height;
+
+    public Person(String name, double age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+}
