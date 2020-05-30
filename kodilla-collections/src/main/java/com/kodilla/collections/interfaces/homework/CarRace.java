8e715ff846e90c73ff3dc371050e96f519ac2d12
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-05-30 17:19:52 +0200

    zadanie 4.1.1.1

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -3,30 +3,20 @@ package com.kodilla.collections.interfaces.homework;
 public class CarRace {
     public static void main(String[] args) {
         Ford ford = new Ford(0);
-        ford.increaseSpeed();
-        ford.increaseSpeed();
-        ford.increaseSpeed();
-        ford.decreaseSpeed();
-        ford.decreaseSpeed();
         doRace(ford);
 
         Opel opel = new Opel(0);
-        opel.increaseSpeed();
-        opel.increaseSpeed();
-        opel.increaseSpeed();
-        opel.decreaseSpeed();
-        opel.decreaseSpeed();
         doRace(opel);
 
         Kia kia = new Kia(0);
-        kia.increaseSpeed();
-        kia.increaseSpeed();
-        kia.increaseSpeed();
-        kia.decreaseSpeed();
-        kia.decreaseSpeed();
         doRace(kia);
     }
     public static void doRace (Car car){
+        car.increaseSpeed();
+        car.increaseSpeed();
+        car.increaseSpeed();
+        car.decreaseSpeed();
+        car.decreaseSpeed();
         System.out.println(car.getSpeed());
     }
 }
