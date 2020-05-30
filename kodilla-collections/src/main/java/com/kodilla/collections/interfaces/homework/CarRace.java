89292c47d724a14a7d60a716e034e1233d262c3b
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-05-30 17:24:13 +0200

    zadanie 4.1.1.1

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/homework/CarRace.java
@@ -4,10 +4,8 @@ public class CarRace {
     public static void main(String[] args) {
         Ford ford = new Ford(0);
         doRace(ford);
-
         Opel opel = new Opel(0);
         doRace(opel);
-
         Kia kia = new Kia(0);
         doRace(kia);
     }
