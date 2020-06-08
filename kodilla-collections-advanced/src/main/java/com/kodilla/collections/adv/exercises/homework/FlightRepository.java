e0155a689e921be0e3319d5c777cc89f0834549d
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-06-08 17:52:03 +0200

    zadanie 5.5

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/exercises/homework/FlightRepository.java
@@ -0,0 +1,16 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class FlightRepository {
+    Map<String, List<Flight>> flights = new HashMap<>();
+
+    public static getFlightsTable(String departure, String arrival){
+        List<Flight> flights = new ArrayList<>();
+        flights.add();
+
+    }
+}
---
+++ kodilla-collections-advanced/src/test/java/com/kodilla/collections/adv/exercises/homework/FlightsTestSuite.java
@@ -0,0 +1,17 @@
+package com.kodilla.collections.adv.exercises.homework;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FlightsTestSuite {
+
+    @Test
+    public void testFindDepartures(){
+        Flight airport1 = new Flight();
+        airport1.getDeparture("Madryt");
+
+        List<Flight> result =
+
+    }
+}
