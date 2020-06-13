b37f2d0b27de7cc6c2b3bae90c425a0656a9efe4
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-06-13 10:56:02 +0200

    zadanie ForumStats

---
+++ src/main/java/com/kodilla/stream/homework/ForumStats.java
@@ -0,0 +1,26 @@
+package com.kodilla.stream.homework;
+
+import com.kodilla.stream.UserRepository;
+
+public class ForumStats {
+    public static void main(String[] args) {
+        double average40 = UserRepository.getUsersList()
+                .stream()
+//                .filter(t -> t.getAge()(>=40))
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(average40);
+
+
+        double averageUnder40 = UserRepository.getUsersList()
+                .stream()
+//                .filter(t -> t.getAge()(<40))
+                .map(u -> u.getNumberOfPosts())
+                .mapToInt(n -> n)
+                .average()
+                .getAsDouble();
+        System.out.println(averageUnder40);
+    }
+}
