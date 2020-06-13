163c826402069d664b3ea88e1c89bc05e7429823
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-06-13 10:20:22 +0200

    zadanie stream 3

--- src/main/java/com/kodilla/stream/homework/TaskRepository.java
+++ src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public class TaskRepository {
     public static List<Task> getTasks(){
-        LocalDate date1 = new LocalDate(2020, 6, 3);
-        LocalDate date2 = new LocalDate(2020, 6,16);
-        LocalDate date3 = new LocalDate(2020,6,15);
+        LocalDate date1 = LocalDate.of(2020, 6, 3);
+        LocalDate date2 = LocalDate.of(2020, 6,16);
+        LocalDate date3 = LocalDate.of(2020,6,15);
+
+        LocalDate currentDate = LocalDate.now();
 
         List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("clean room",date1, date1));
+        tasks.add(new Task("clean room",date1, currentDate));
         tasks.add(new Task("call grandpa",date2, date2));
         tasks.add(new Task("do homework",date3,date2));
         return tasks;
