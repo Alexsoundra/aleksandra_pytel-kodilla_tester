ec7a7cc99699b036b9a261374edc94be93ecab68
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-06-17 21:51:53 +0200

    zadanie stream 2

--- src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -8,13 +8,8 @@ public class TaskManager {
     public static void main(String[] args) {
         List<LocalDate> deadline = TaskRepository.getTasks()
                 .stream()
-                .filter(t -> t.getDeadline())
-                .map(TaskManager::getName)
-                .isAfter(LocalDate.now())
-                .forEach(t -> System.out.println(t));
-
-    }
-    public static String getName (Task task){
-        return task.getName();
+                .filter(t -> t.getDeadline().isAfter(LocalDate.now()))
+                .map(task -> task.getDeadline())
+                .collect(Collectors.toList());
     }
 }
