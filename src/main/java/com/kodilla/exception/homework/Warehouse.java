d492b3a6205b82df542c84176a6736b2abe97f57
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-06-20 12:58:53 +0200

    zadanie order

--- src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -2,20 +2,26 @@ package com.kodilla.exception.homework;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 
 public class Warehouse {
+    List<Order> orders = new ArrayList<>();
 
-   List<Order> orders = Order.getNumber();
 
-    public List<Order> addOrder(Order order){
-        List<Order> orders = new ArrayList<>();
-            for(Order order : orders){
-                orders.add(order);
-            }
-        return (List<Order>) order;
+    public void addOrder(Order order){
+        orders.add(order);
     }
 
-    public Order getOrder(String name){
+    public Order getOrder(String name) throws OrderDoesntExistException {
+              Optional<Order> optionalOrder = orders
+                .stream()
+                .filter(order -> order.getNumber().equals(name))
+                .findFirst();
 
+              Order orderX = optionalOrder.orElseThrow(()-> {
+                  return new OrderDoesntExistException();
+              });
+              return orderX;
     }
 }
--- src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -1,14 +1,25 @@
 package com.kodilla.exception.homework;
 
+import com.kodilla.exception.AirportNotFoundException;
+
 import java.util.HashMap;
 import java.util.List;
 
 
 public class WarehouseApp {
     public static void main(String[] args) {
-        List<Order> getListOfOrders(){
-            List<Order> orders = new HashMap<>();
-            orders.add()
+
+        Warehouse orders = new Warehouse();
+        orders.addOrder(new Order("16"));
+        orders.addOrder(new Order("123"));
+        orders.addOrder(new Order("25"));
+
+
+        try{
+            Order isOrderInWarehouse = orders.getOrder("15");
+            System.out.println("Order status: " + isOrderInWarehouse);
+        } catch (OrderDoesntExistException e) {
+            System.out.println("This order doesn't exist");
         }
 
     }
