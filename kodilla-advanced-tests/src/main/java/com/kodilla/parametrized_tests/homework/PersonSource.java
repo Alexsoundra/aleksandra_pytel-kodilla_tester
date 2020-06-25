8d5623c53a09ad798931d48494ff3f28e7c05013
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-06-25 19:49:30 +0200

    zadanie 7.3 fin

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/homework/PersonSource.java
@@ -0,0 +1,22 @@
+package com.kodilla.parametrized_tests.homework;
+
+import java.util.stream.Stream;
+
+public class PersonSource {
+
+    static Stream<Arguments> shouldCalculateBMI(){
+        return Stream.of(
+                Arguments.of(1.80, 45, "Very severely underweight"),
+                Arguments.of(1.60, 40.5, "Severely underweight"),
+                Arguments.of(1.75, 56, "Underweight"),
+                Arguments.of(1.70, 60, "Normal (healthy weight)"),
+                Arguments.of(1.60, 65, "Overweight"),
+                Arguments.of(1.60, 77, "Obese Class I (Moderately obese)"),
+                Arguments.of(1.70, 110, "Obese Class II (Severely obese)"),
+                Arguments.of(1.70, 130, "Obese Class IV (Morbidly Obese)"),
+                Arguments.of(1.70, 150, "Obese Class V (Super Obese)"),
+                Arguments.of(1.70, 200, "Obese Class VI (Hyper Obese)")
+        );
+    }
+
+}
