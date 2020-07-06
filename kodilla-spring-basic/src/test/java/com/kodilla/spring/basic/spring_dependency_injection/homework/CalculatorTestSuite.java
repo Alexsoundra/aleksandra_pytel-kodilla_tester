9f7b29b71975eadb1ddc4b7c0763b1c054e3b122
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-07-06 19:46:38 +0200

    9.2 start

---
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CalculatorTestSuite {
+
+    @Test
+    public void shouldReturnResultOfAddition(){
+
+    }
+
+}
