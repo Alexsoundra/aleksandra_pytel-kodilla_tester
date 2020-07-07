9fee055d86fe5d132e78b783af3152dabc9f733f
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-07-07 18:46:53 +0200

    9.2

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
@@ -1,6 +1,4 @@
 package com.kodilla.spring.basic.spring_dependency_injection.homework;
-
-
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,17 +10,26 @@ public class Calculator {
         this.display = display;
     }
 
+
     public double add(double a, double b){
 
-        return a + b;
+        double result = a + b;
+        display.display(result);
+        return result;
     }
     public double subtract(double a, double b){
-        return a - b;
+        double result = a -b;
+        display.display(result);
+        return result;
     }
     public double multiply(double a, double b){
-        return a * b;
+        double result = a * b;
+        display.display(result);
+        return result;
     }
     public double divide(double a, double b){
-        return a / b;
+        double result = a / b;
+        display.display(result);
+        return result;
     }
 }
--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_dependency_injection/homework/CalculatorTestSuite.java
@@ -7,10 +7,38 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CalculatorTestSuite {
+    Display display = new Display();
+    Calculator calculator = new Calculator(display);
 
     @Test
     public void shouldReturnResultOfAddition(){
-
+        double a = 5.2;
+        double b = 8.0;
+        double sumResult = calculator.add(a, b);
+        assertEquals(13.2, sumResult);
     }
 
+    @Test
+    public void shouldReturnResultOfSubtraction(){
+        double a = 9.1;
+        double b = 8.1;
+        double result = calculator.subtract(a, b);
+        assertEquals(1.0, result);
+    }
+
+    @Test
+    public void shouldReturnResultOfMultiplication(){
+        double a = 3.0;
+        double b = 1.5;
+        double result = calculator.multiply(a, b);
+        assertEquals(4.5, result);
+    }
+
+    @Test
+    public void shouldReturnResultOfDivision(){
+        double a = 8.2;
+        double b = 2.0;
+        double result = calculator.divide(a, b);
+        assertEquals(4.1, result);
+    }
 }
