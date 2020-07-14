a5877f9eaf9fe4be8773f33d97f38e298494a13b
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-07-14 18:49:19 +0200

    9.5 finish

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -3,14 +3,21 @@ package com.kodilla.spring.basic.spring_scopes.homework;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Component
 @Scope("prototype")
 public class Clock {
 
+    private LocalDateTime localDateTime;
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
     public Clock(){
-        System.out.println(LocalTime.now());
+       localDateTime = LocalDateTime.now();
     }
 
 }
--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_scopes/homework/ClockTestSuite.java
@@ -5,17 +5,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.time.LocalDateTime;
+
 
 class ClockTestSuite {
 
     @Test
-    public void shouldShowDifferentTime() {
+    public void shouldShowDifferentTime() throws InterruptedException {
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         Clock firstBean = context.getBean(Clock.class);
+        LocalDateTime time1 = firstBean.getLocalDateTime();
+        Thread.sleep(500);
         Clock secondBean = context.getBean(Clock.class);
+        LocalDateTime time2 = secondBean.getLocalDateTime();
+        Thread.sleep(500);
         Clock thirdBean = context.getBean(Clock.class);
-        Assertions.assertNotEquals(firstBean, secondBean);
-        Assertions.assertNotEquals(secondBean, thirdBean);
-        Assertions.assertNotEquals(firstBean, thirdBean);
+        LocalDateTime time3 = thirdBean.getLocalDateTime();
+        Assertions.assertNotEquals(time1, time2);
+        Assertions.assertNotEquals(time2, time3);
+        Assertions.assertNotEquals(time1, time3);
     }
 }
