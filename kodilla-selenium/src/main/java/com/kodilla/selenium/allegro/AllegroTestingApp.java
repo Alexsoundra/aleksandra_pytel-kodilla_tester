d5ef1bfdaa56c939c892d07eaaf1ae7970c165ac
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-08-04 18:39:00 +0200

    allegrocss2

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -14,20 +14,19 @@ public class AllegroTestingApp {
         WebDriver driver = new ChromeDriver();
         driver.get("https://allegro.pl");
 
-//        driver.switchTo().alert().accept();
 
         WebElement closeAlert = driver.findElement(By.cssSelector("div._9f0v0._jkrtd.mpof_ki_s > button._13q9y._8hkto.munh_56_m.m7er_k4.m7er_wn.m7er_56_m"));
-        closeAlert.submit();
+        closeAlert.click();
 
         WebElement inputField = driver.findElement(By.name("string"));
         inputField.sendKeys("Mavic mini");
         inputField.submit();
 
-        WebElement categoryCombo = driver.findElement(By.cssSelector("div.mr3m_1 m7er_k4 m7er_k4 m7er_wn _k70df mgn2_14 mgn2_14 mgn2_jk mp0t_0a mqu1_21 mgmw_wo mli8_k4 _d25db_an94v > option"));
+        WebElement categoryCombo = driver.findElement(By.cssSelector("div.mr3m_1 m7er_k4 m7er_k4 m7er_wn _k70df mgn2_14 mgn2_14 mgn2_jk mp0t_0a mqu1_21 mgmw_wo mli8_k4 _d25db_an94v > select"));
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByIndex(3);
 
-       List<WebElement> elements = (List<WebElement>) driver.findElement(By.className("_9c44d_3pyzl"));
+       List<WebElement> elements = driver.findElements(By.className("_9c44d_3pyzl"));
         elements.get(0).getText();
     }
 }
