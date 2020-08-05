cc2ff5cb402efad12b98e0ae31885aae05cc23b6
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-08-05 19:12:44 +0200

    pom

---
+++ kodilla-google-selenium/src/main/java/pages/GoogleSearch.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class GoogleSearch extends AbstractPage{
+    @FindBy(css = "input[title='Szukaj']")
+    static WebElement inputField;
+
+    @FindBy(css = "input[value='Szukaj w Google']")
+    static List<WebElement> searchButton;
+    private static GoogleResults googleResults;
+
+    public GoogleSearch(WebDriver driver) {
+        super(driver);
+    }
+
+    public void searchResults() {
+        PageFactory.initElements(driver, GoogleSearch.class);
+        inputField.sendKeys("Kodilla");
+        googleResults = loadResults(driver);
+        googleResults.iSeeResults();
+    }
+
+
+    public static GoogleResults loadResults(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.elementToBeClickable(searchButton.get(0))).click();
+        GoogleResults googleResults = new GoogleResults(driver);
+        return googleResults;
+    }
+
+    public void checkResults() {
+        WebElement check = driver.findElement(By.className("LC20lb DKV0Md"));
+        check.submit();
+    }
+}
