06d6baea7797a32f43da987643c5363c456870e7
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-08-05 19:01:20 +0200

    allegro3

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestApp.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AllegroTestApp {
     public static void main(String[] args) {
@@ -16,7 +18,10 @@ public class AllegroTestApp {
         WebElement closeAlert = driver.findElement(By.cssSelector("div._9f0v0._jkrtd.mpof_ki_s > button._13q9y._8hkto.munh_56_m.m7er_k4.m7er_wn.m7er_56_m"));
         closeAlert.click();
 
-        WebElement inputField = driver.findElement(By.name("string"));
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.invisibilityOfElementLocated((By.id("#dialog-content"))));
+
+        WebElement inputField = driver.findElement(By.cssSelector("form > input"));
         inputField.sendKeys("Mavic mini");
         inputField.submit();
 
@@ -24,7 +29,7 @@ public class AllegroTestApp {
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByIndex(4);
 
-        inputField = driver.findElement(By.xpath("//html/body/div/header/div/div/div/div/form/div/button"));
-        inputField.click();
+        WebElement button = driver.findElement(By.xpath("//html/body/div/header/div/div/div/div/form/div/button"));
+        button.click();
     }
 }
