91e85c98d18f88793e58282be3e8e43120a73db6
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-08-20 10:01:14 +0200

    15.2 year

--- kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
+++ kodilla-code-coverage/src/test/java/com/kodilla/jacoco/YearTest.java
@@ -1,6 +1,8 @@
 package com.kodilla.jacoco;
 
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class YearTest {
@@ -17,4 +19,51 @@ public class YearTest {
         //then
         assertTrue(isLeapYear);
     }
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy400() {
+        //given
+        Year year = new Year(1602);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy100() {
+        //given
+        Year year = new Year(2000);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertTrue(isLeapYear);
+    }
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy100() {
+        //given
+        Year year = new Year(2003);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertFalse(isLeapYear);
+    }
+    @Test
+    public void shouldNotBeLeapYearIfDivisibleBy4(){
+        //given
+        Year year = new Year(2003);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertFalse(isLeapYear);
+    }
+    @Test
+    public void shouldBeLeapYearIfDivisibleBy4(){
+        //given
+        Year year = new Year(2000);
+        //when
+        boolean isLeapYear = year.isLeap();
+        //then
+        assertTrue(isLeapYear);
+    }
+
 }
