2763bb41f622883965e802508ba11264fb47f760
Aleksandra Pytel <alexsoundra@mixbox.pl>
2020-08-21 09:23:45 +0200

    fizzbuzz

--- kodilla-cucumber/src/main/java/fizzbuzzchecker/FizzBuzzChecker.java
+++ kodilla-cucumber/src/main/java/fizzbuzzchecker/FizzBuzzChecker.java
@@ -7,7 +7,7 @@ public class FizzBuzzChecker {
             return "Fizz";
         }else if(number % 5 ==0){
             return "Buzz";
-        }else if(number % 3==0) && (number % 5){
+        }else if(number % 3==0 && number % 5==0){
             return "FizzBuzz";
         }else{
             return "Nope";}
